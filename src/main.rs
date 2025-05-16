a00de23ddb105c67be4696cd2e545bdaed803b77
bem130 <bem130@ymail.ne.jp>
2025-05-16 23:20:16 +0900

    カウントの改良 コメント追加

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
+// [問題]
+// 2行5列の床に赤青黄のタイルを配置する。
+// 1. 赤4枚、青4枚、黄2枚を使う。
+// 2. 同じ色のタイルが隣接しないように配置する。
+// 3. 全ての配置パターンを表示する。
+// 4. 適合するパターンの数を表示する。
 
 fn main() {
     let mut count = 0; // 適合したパターンの数
     'brute_force: for i in 0..59049 { // 全パターン 総当たり
-        // 床パターンを生成
+        let mut count_red = 0;
+        let mut count_blue = 0;
+        let mut count_yellow = 0;
+        // 床パターンを生成 しつつタイルをカウント
         let mut floor = Floor::new();
         for r in 0..2 {
             for c in 0..5 {
                 floor.tiles[r][c] = match nth!(i, r*5+c+1, 3) {
-                    0 => Tile::Red,
-                    1 => Tile::Blue,
-                    2 => Tile::Yellow,
+                    0 => {
+                        count_red += 1;
+                        Tile::Red
+                    },
+                    1 => {
+                        count_blue += 1;
+                        Tile::Blue
+                    },
+                    2 => {
+                        count_yellow += 1;
+                        Tile::Yellow
+                    },
                     _ => unreachable!(),
                 };
             }
         }
         // タイルの枚数を確認
-        if floor.count(Tile::Red   ) != 4 { continue 'brute_force; }
-        if floor.count(Tile::Blue  ) != 4 { continue 'brute_force; }
-        if floor.count(Tile::Yellow) != 2 { continue 'brute_force; }
+        if count_red != 4 || count_blue != 4 || count_yellow != 2 { continue 'brute_force; }
         // タイルの配置を確認
         for r in 0..2 {
             for c in 0..5 {
@@ -63,18 +79,6 @@ impl Floor {
 
     // タイルの条件
 
-    fn count(&self, tile: Tile) -> usize {
-        let mut count = 0;
-        for row in 0..2 {
-            for col in 0..5 {
-                if self.tiles[row][col] == tile {
-                    count += 1;
-                }
-            }
-        }
-        count
-    }
-
     fn check_adjacent(&self, row: usize, col: usize) -> bool {
         let tile = self.tiles[row][col];
         // 上下左右の隣接を確認
@@ -112,11 +116,8 @@ impl Floor {
 // 整数xのbase進数でのk桁目を取得するマクロ
 
 #[macro_export]
-macro_rules! nth { // 整数xのk桁目
+macro_rules! nth {
     ($x:expr, $k:expr, $base:expr) => {{
         ($x / ($base as i32).pow((($k - 1) as u32))) % $base
-    }};
-    ($x:expr, $k:expr) => {{ // base省略したら10進数
-        ($x / 10_i32.pow((($k - 1) as u32))) % 10
     }};
 }
